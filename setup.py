38cb9fa0e802dfdb085754437f60e1eda9ccb7be
mmalates <malatesta.mike@gmail.com>
2017-12-06 09:03:42 -0800

    working predictor class with load data, dummify, feature set, feature selection, fit and predict

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ds_utils',
+      version='0.1',
+      description='object-oriented framework for model building',
+      url='http://github.com/mmalates/ds_utils',
+      author='Mike Malatesta',
+      author_email='malatesta.mike@gmail.com',
+      license='MIT',
+      packages=['ds_utils'],
+      zip_safe=False)
